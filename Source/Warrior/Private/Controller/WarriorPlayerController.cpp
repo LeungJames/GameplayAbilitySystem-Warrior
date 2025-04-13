ba9a87cf76b6b0dfbd24647a31ac2ff76f3ee109
LeungJames <1801240480@qq.com>
2025-04-13 14:46:06 +0800

    Set up movement

--- Source/Warrior/Private/Controller/WarriorPlayerController.cpp
+++ Source/Warrior/Private/Controller/WarriorPlayerController.cpp
@@ -2,4 +2,39 @@
 
 
 #include "Controller/WarriorPlayerController.h"
+#include "GameFramework/Pawn.h"
+#include "Engine/LocalPlayer.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+void AWarriorPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&ThisClass::Move);
+	EnhancedInputComponent->BindAction(LookAction,ETriggerEvent::Triggered,this,&ThisClass::Look);
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(DefaultInputMappingContext,0);
+	}
+	
+}
+
+void AWarriorPlayerController::Move(const FInputActionValue& InputValue)
+{
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+	FRotator ControlYawRotation = FRotator(0,GetControlRotation().Yaw,0);
+	FVector Forward = FRotationMatrix(ControlRotation).GetUnitAxis(EAxis::X);
+	FVector Right = FRotationMatrix(ControlRotation).GetUnitAxis(EAxis::Y);
+	GetPawn()->AddMovementInput(Forward,InputVector.Y);
+	GetPawn()->AddMovementInput(Right,InputVector.X);
+}
+
+void AWarriorPlayerController::Look(const FInputActionValue& InputValue)
+{
+	FVector2D LookInputVector = InputValue.Get<FVector2D>();
+	GetPawn()->AddControllerYawInput(LookInputVector.X);
+	GetPawn()->AddControllerPitchInput(LookInputVector.Y);
+}
--- Source/Warrior/Public/Controller/WarriorPlayerController.h
+++ Source/Warrior/Public/Controller/WarriorPlayerController.h
@@ -4,14 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputAction.h"
 #include "WarriorPlayerController.generated.h"
 
-/**
- * 
- */
+class UInputMappingContext;
+struct FInputActionValue;
+
 UCLASS()
 class WARRIOR_API AWarriorPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+	void Move(const FInputActionValue& InputValue);
+	void Look(const FInputActionValue& InputValue);
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category = "Input", meta=(AllowPrivateAccess=true))
+	TObjectPtr<UInputMappingContext> DefaultInputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category = "Input", meta=(AllowPrivateAccess=true))
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly, Category = "Input", meta=(AllowPrivateAccess=true))
+	UInputAction* LookAction;
 };
